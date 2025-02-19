ee02f5724f49993304cd3d54c505b777c0bc4503
mergd <24722014+mergd@users.noreply.github.com>
2025-02-19 13:12:28 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -60,11 +60,11 @@ export default function Home() {
                 </div>
               </div>
             )}
-            <p className="text-xs pt-2 text-gray-500 dark:text-gray-400">
-              OTP hasn&apos;t been updated? wait for at least 1 minute before
-              re-requesting another OTP.
-            </p>
           </div>
+          <p className="text-xs pt-2 text-gray-500 dark:text-gray-400">
+            OTP hasn&apos;t been updated? wait for at least 1 minute before
+            re-requesting another OTP.
+          </p>
         </div>
       </main>
     </div>
